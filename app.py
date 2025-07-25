88228d936924c2580146d35f542f490953ef5a8f
Giovane Cordelini <giovane.cordelini@gmail.com>
2025-07-25 16:35:23 -0300

    versão final com interrupção, campos dinâmicos e layout melhorado

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ from processamento import processar_planilha
 
 st.set_page_config(page_title="Envio de Transações CAF", layout="centered")
 
-# Inicializar os estados se não existirem
-for key in ["iniciar_envio", "envio_em_andamento", "interromper"]:
+# Inicializa os estados obrigatórios
+for key in ["iniciar_envio", "envio_em_andamento", "interromper", "arquivo", "auth_token", "template_id"]:
     if key not in st.session_state:
-        st.session_state[key] = False
+        st.session_state[key] = None if key == "arquivo" else False
 
-# Renderiza interface
+# Renderiza a interface
 render_layout()
 
-# Processa planilha se necessário
-processar_planilha(
-    arquivo=st.session_state.get("arquivo"),
-    auth_token=st.session_state.get("auth_token"),
-    template_id=st.session_state.get("template_id")
-)
+# Só chama processamento se todos os campos estiverem preenchidos
+if (
+    st.session_state["iniciar_envio"]
+    and st.session_state["arquivo"]
+    and st.session_state["auth_token"]
+    and st.session_state["template_id"]
+):
+    processar_planilha(
+        arquivo=st.session_state["arquivo"],
+        auth_token=st.session_state["auth_token"],
+        template_id=st.session_state["template_id"]
+    )
